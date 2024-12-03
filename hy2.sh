886712ba67a22e279fa092f9e994f86523b406a2
Heroyessj <153788917+Heroyessj@users.noreply.github.com>
2024-12-03 09:39:21 +0800

    Update hy2.sh

--- hy2.sh
+++ hy2.sh
@@ -26,7 +26,7 @@ tls:
 
 auth:
   type: password
-  password: $GENPASS
+  password: S123456j
 
 masquerade:
   type: proxy
@@ -80,7 +80,7 @@ service hysteria start
 
 echo "------------------------------------------------------------------------"
 echo "hysteria2已经安装完成"
-echo "默认端口： 49616 ， 密码为： $GENPASS ，工具中配置：tls，SNI为： bing.com"
+echo "默认端口： 49616 ， 密码为： S123456j ，工具中配置：tls，SNI为： bing.com"
 echo "配置文件：/etc/hysteria/config.yaml"
 echo "已经随系统自动启动"
 echo "看状态 service hysteria status"
